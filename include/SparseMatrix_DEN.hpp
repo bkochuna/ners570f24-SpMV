5917e1681b264eb6958ed5faef394776f39bcb71
Andrew Paul Panter <panteran@umich.edu>
2024-10-18 18:36:17 -0400

    constructor/destructor ready for review

--- include/SparseMatrix_DEN.hpp
+++ include/SparseMatrix_DEN.hpp
@@ -9,11 +9,13 @@ namespace SpMV
     class SparseMatrix_DEN : public SparseMatrix<fp_type>
     {
     private:
-        std::vector<std::vector<fp_type>> A_mat;
+        std::vector<std::vector<fp_type>> _A_mat;
 
     public:
         SparseMatrix_DEN(const int nrows, const int ncols);
         SparseMatrix_DEN();
         ~SparseMatrix_DEN();
+        void assembleStorage() {}; //placeholder
+        void disassembleStorage() {}; //placeholder
     }; // class SparseMatrix_DEN
 } // namespace SpMV
--- src/SparseMatrix_DEN.cpp
+++ src/SparseMatrix_DEN.cpp
@@ -9,22 +9,28 @@ namespace SpMV
          SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
     {
         std::cout << "Hello from SparseMatrix_DEN Constructor!\n";
-        this->nrows = nrows;
-        this->ncols = ncols;
+        this->_nrows = nrows;
+        this->_ncols = ncols;
         assembleStorage();
 
     }
 
+    template <class fp_type>
     SparseMatrix_DEN<fp_type>::SparseMatrix_DEN() :
-         SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+         SparseMatrix<fp_type>::SparseMatrix(1, 1)
     {
         std::cout << "Hello from SparseMatrix_DEN Default Constructor!\n";
-        this->nrows = 1;
-        this->ncols = 1;
+        this->setCoefficient(0, 0, 0.0);
         assembleStorage();
 
     }
 
+    template <class fp_type>
+    SparseMatrix_DEN<fp_type>::~SparseMatrix_DEN()
+    {
+        std::cout << "Hello from SparseMatrix_DEN Destuctor!\n";
+        disassembleStorage();
+    }
 
     template class SparseMatrix_DEN<float>;
     template class SparseMatrix_DEN<double>;
