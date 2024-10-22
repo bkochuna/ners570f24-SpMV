1cb018eae55f6e0a55c6f140eefd1710e09a9e0e
Declan Brick <brickd@umich.edu>
2024-10-22 17:12:55 -0400

    Add unit tests for accessor methods

--- tests/example_unit_test.cpp
+++ tests/example_unit_test.cpp
@@ -1,6 +1,7 @@
-#include <SpMV.hpp>
+#include "SpMV.hpp"
 
 #include <vector> // std::vector
+#include <float.h> //need for DBL_MAX and DBL_MIN
 
 // Testing library required for testing (Always include this last!)
 #include "unit_test_framework.hpp"
@@ -46,11 +47,90 @@ TEST_CASE(compareVectors)
   }
 } // compareVectors
 
+//Unit Tests for the accessor methods
+TEST_CASE(ACCESSORS)
+{
+
+  //randomly choose number of rows and columns
+  // 1000 is upper limit to prevent test from running too long
+  // 4 is lower limit to ensure 10% density has at least one element to fill
+  srand(static_cast<unsigned int>(time(NULL)));
+  size_t R = rand() % 1000 + 4;
+  size_t C = rand() % 1000 + 4;
+
+  //initialize the matrix
+  SpMV::SparseMatrix_DEN<double> A = SpMV::SparseMatrix_DEN<double>(int(R),int(C));
+
+  //randomly assign the values
+  //initialize using COO format
+  size_t n_elements = R*C;
+  size_t n_nonzero = size_t(double(n_elements) * 0.1);//want 90% of matrix as sparse (sizing arbitrary)
+  size_t *R_idx = new size_t[n_nonzero];
+  size_t *C_idx = new size_t[n_nonzero];
+  double *values = new double[n_nonzero];
+  bool has_duplicates; 
+  bool is_zero;
+
+  //uniformly sample without replacement
+  for (size_t i = 0; i < n_nonzero; ++i){
+    values[i] = (rand() / double(RAND_MAX)) * DBL_MAX + DBL_MIN;//choose random value between the double min and max
+    R_idx[i] = rand() % R;//choose random value between 0 and R
+    C_idx[i] = rand() % C;//choose random value between 0 and C
+    has_duplicates = true;//assume we have duplicates
+    //check for duplicates
+    while (has_duplicates){
+      //set the flag to false so we only go through once if needed
+      has_duplicates = false; 
+      //check each element of the array that has already been set
+      for (size_t ii = 0; ii < i; ++ii){
+        //if the row is duplicated
+        if (R_idx[i] == R_idx[ii] && C_idx[i] == C_idx[ii]){
+          //resample C
+          C_idx[i] = rand() % C;
+          //flag that a duplicate was found and exit to outer loop to check again
+          has_duplicates = true;
+          break;
+        }
+      }
+    }
+  }
+
+  //assign
+  //update call after method is implemented
+  A.assembleStorage();
+
+  //check the n_rows and n_columns accessors 
+  ASSERT(R == A.getNumRows());
+  ASSERT(C == A.getNumCols());
+
+  //loop over all elements
+  for (size_t i = 0; i < R; ++i){
+    for(size_t j = 0; j < C; ++j){
+      is_zero = true;
+      //check if the value is non zero. If so, compare
+      for(size_t ii = 0; ii < n_nonzero; ++ii){
+        if (i == R_idx[ii] && j == C_idx[ii]){
+          ASSERT_NEAR(values[ii], A.getCoefficient(i, j), 1e-3);
+          is_zero = false;
+        }
+      }
+      if (is_zero){ASSERT_NEAR(0.0, A.getCoefficient(i, j), 1e-3);}
+    }
+  }
+
+  //cleanup 
+  delete[] R_idx;
+  delete[] C_idx;
+  delete[] values;
+
+}
+
 // Create a test suite
 TEST_SUITE(my_suite) 
 {
   // Run the unit test when the suite is run
   TEST(compareVectors);
+  TEST(ACCESSORS);
 } // my_suite
 
 // We can also create templated tests and suites
