9c7a7f000077782e5d5030d296dccfa210b63fb2
Athena Liu <athenafh@gl-login3.arc-ts.umich.edu>
2024-10-22 22:43:09 -0400

    Add accessor methods for the four private attributes

--- include/SparseMatrix_ELL.hpp
+++ include/SparseMatrix_ELL.hpp
@@ -39,4 +39,28 @@ namespace SpMV
         void view() {assert(false);}                            // View
 
     }; // class SparseMatrix_ELL
+
+    // Accessor methods
+    
+    // Accessor for lmax
+    size_t getLmax() const {
+        return lmax;
+    }
+
+    // Accessor for nnzs
+    size_t getNnzs() const {
+        return nnzs;
+    }
+
+    // Accessor for colIdx
+    const size_t* getColIdx() const {
+        return colIdx; // Return pointer to colIdx
+    }
+
+    // Accessor for val
+    const fp_type* getVal() const {
+        return val; // Return pointer to val
+    }
+
+
 } // namespace SpMV
