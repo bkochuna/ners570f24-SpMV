ad10d17a7308ed30d727ee06a302c81568ac4fe7
shreyasv11 <64632587+shreyasv11@users.noreply.github.com>
2024-10-23 12:38:21 -0400

    Update SparseMatrix_CSR.hpp
    Adding the constructor and destructor headers

--- include/SparseMatrix_CSR.hpp
+++ include/SparseMatrix_CSR.hpp
@@ -13,7 +13,13 @@ namespace SpMV{
         std::vector<fp_type> values;
 
     public:
+        SparseMatrix_CSR();
         SparseMatrix_CSR(const size_t nrows, const size_t ncols);
+        SparseMatrix_CSR(const SparseMatrix_CSR& other);
+        SparseMatrix_CSR(SparseMatrix_CSR&& other) noexcept;
+        ~SparseMatrix_CSR() = default;
+        SparseMatrix_CSR& operator=(const SparseMatrix_CSR& other);
+        SparseMatrix_CSR& operator=(SparseMatrix_CSR&& other) noexcept;
       
         // Access the base matrix attributes from the Sparse Matrix Class
         inline size_t      getNumNonZeros() const { return this-> _nnz;}
