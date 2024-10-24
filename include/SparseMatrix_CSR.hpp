1dda355f2c2bb643efdb88efd7c2e16a5d048c02
Matthew Granados <magranad@gl-login1.arc-ts.umich.edu>
2024-10-23 23:00:46 -0400

    Added getCSRvalue accessor; CSR cpp and hpp updated

--- include/SparseMatrix_CSR.hpp
+++ include/SparseMatrix_CSR.hpp
@@ -16,17 +16,20 @@ namespace SpMV{
         SparseMatrix_CSR(const size_t nrows, const size_t ncols);
       
         // Access the base matrix attributes from the Sparse Matrix Class
-        inline size_t      getNumNonZeros() const { return this-> _nnz;}
-        inline size_t      getNRows()       const { return this-> _nrows;}
-        inline size_t      getNCols()       const { return this-> _ncols;}
+        inline size_t      getNumNonZeros() const { return this-> _nnz; }
+        inline size_t      getNRows()       const { return this-> _nrows; }
+        inline size_t      getNCols()       const { return this-> _ncols; }
 
         // Access the attributes from the CSR Class
-        inline const std::vector<size_t>   getRowIdx() const { return rowIdx; }
-        inline const std::vector<size_t>   getColIdx() const { return colIdx; }
-        inline const std::vector<fp_type>  getValues() const { return values; }
+        inline const std::vector<size_t>   getRowIdx() const { return this-> rowIdx; }
+        inline const std::vector<size_t>   getColIdx() const { return this-> colIdx; }
+        inline const std::vector<fp_type>  getValues() const { return this-> values; }
 
         void viewCSR(); // View the CSR representation and output the full matrix
         void assembleStorage(); // Declare the implementation
         void disassembleStorage(); // Declare the implementation
+
+        // Accessor to grab a value given a row + col idx
+        fp_type getCSRvalue(const size_t row, const size_t col) const; 
     }; // class SparseMatrix_COO
 } // namespace SpMV
