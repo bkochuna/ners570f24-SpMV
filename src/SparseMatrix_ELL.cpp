2804f42bfb2b2dc819ac89fce9ba031cf7b32eed
Athena Liu <athenafh@gl-login1.arc-ts.umich.edu>
2024-10-24 21:17:58 -0400

    Implement accessor method for getValue with given index

--- src/SparseMatrix_ELL.cpp
+++ src/SparseMatrix_ELL.cpp
@@ -44,6 +44,46 @@ namespace SpMV
         std::cout << "Byebye from SparseMatrix_ELL Destructor~\n";
     }
 
+     // Accessor of value
+    template <class fp_type>
+    fp_type SparseMatrix_ELL<fp_type>::getELLvalue(const size_t row, const size_t col) const
+    {
+	// Initialize
+	fp_type ELLvalue = 0;
+
+	// Check if the matrix is assembled
+	if (this->_state==assembled) {
+
+		// Determine if the value is found
+		size_t isFound = 0;
+
+		// Check if row and col are valid
+		if (col > this->_ncols || row > this->_nrows) {
+			throw std::invalid_argument("Error: Indices out of bound!");
+		}
+
+		// Find value corresponding to (row,col)
+		for (size_t i = this->lmax * row; i < this->lmax * (row+1): ++i) {
+			if (this->colIdx[i] == col && this->val[i] > 0) {
+				isFound++;
+				ELLvalue = val[i];
+				break;
+			}
+		}
+
+		// No value found
+		if (isFound == 0) {
+			std::cout << "No value assigned at (" << row << "," << col << ")" << std::endl;
+		}
+	}
+	else {
+		throw std::runtime_error("Invalid matrix state: matrix is not assembled!");
+	}
+
+	return ELLvalue;
+    }
+
+
     template <class fp_type>
     void SparseMatrix_ELL<fp_type>::view()
     {
